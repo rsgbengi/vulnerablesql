f476e0b29e775f11108e0a1e425aa85ccd2ac699
rsgbengi <ruben.santosg@gmail.com>
2021-10-05 20:41:57 +0200

    cambios fotos

--- LFI/Gatitos/static/js/script.js
+++ LFI/Gatitos/static/js/script.js
@@ -1,4 +1,4 @@
 function gatoboton() {
     var numero = Math.floor((Math.random() * 50) + 1);
-    location.href = window.location.origin + window.location.pathname + "?foto=" + numero;
+    location.href = window.location.origin + window.location.pathname + "?foto=" + numero + ".jpg";
 }
